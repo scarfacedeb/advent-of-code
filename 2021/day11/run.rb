b855646a398f2f03e071a88c50ed2ab81c72b550
Andrew Volozhanin <scarfacedeb@gmail.com>
2021-12-11 09:06:32 +0100

    Add day 11 animation

--- 2021/day11/run.rb
+++ 2021/day11/run.rb
@@ -1,20 +1,43 @@
 require 'benchmark'
+require 'colorize'
 
 def read_input
   file = $all ? 'input.txt' : 'example.txt'
-  input = File.read(ARGV.last || file).split("\n")
+  input = File.read(ARGV[-3] || file).split("\n")
   input.map { _1.split('').map(&:to_i) }
 end
 
-p Benchmark.measure {
+# p Benchmark.measure {
 
 ADJACENT = [-1, 1, 0].repeated_permutation(2).reject { _1 == [0, 0] }.sort
 
 octos = read_input
 
-def draw(label = nil, octos)
-  puts "=== #{label}" if label
-  puts octos.map { _1.map { |e| e > 9 ? '*' : e }.join }.join("\n")
+def animate(st, flashing, octos)
+  delay = ARGV[-2].to_f
+
+  before, after = false, true
+  3.times { |f|
+    after = draw("STEP #{st}", octos, f)
+    if before != after
+      system 'clear'
+      puts after
+      before = after
+    end
+    sleep delay
+  }
+end
+
+def draw(label, octos, f)
+  "  #{label}\n" + octos.map { _1.map { |e| color(e, f) }.join }.join("\n")
+end
+
+def color(e, f)
+  case e
+  when 1..8 then e.to_s.colorize(:white)
+  when 9 then e.to_s.colorize(f < 1 ? :white : :yellow)
+  when 0 then '*'.colorize(f < 1 ? :yellow : :red)
+  end
 end
 
 def step(octos, pos, from = nil)
@@ -43,7 +66,6 @@ loop do
     end
   end
 
-  # draw "STEP #{st + 1}", octos
   flashing = 0
 
   rows.times do |i|
@@ -55,6 +77,8 @@ loop do
     end
   end
 
+  animate st, flashing, octos if st > ARGV[-1].to_i
+
   total += flashing if st <= 100
   break st if flashing == octos_count
 
@@ -63,4 +87,4 @@ end
 
 puts "Answer 11.1: #{total}"
 puts "Answer 11.2: #{st}"
-}.total * 1000
+# }.total * 1000
