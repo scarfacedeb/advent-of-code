370e348c08102e3f46252c0cf7976d2f002618f7
Andrew Volozhanin <scarfacedeb@gmail.com>
2022-12-05 06:53:21 +0100

    2022 / Day 5 / improvements

--- 2022/day5/run.rb
+++ 2022/day5/run.rb
@@ -4,13 +4,14 @@ def read_input
 end
 
 def move(stacks, m, order = nil)
-  stacks[m[:from]].shift(m[:cnt]).then { stacks[m[:to]].unshift(*(order == :stack ? _1.reverse : _1)) }
+  cnt, from, to = m
+  stacks[from].shift(cnt).then { stacks[to].unshift(*(order == :stack ? _1.reverse : _1)) }
 end
 
 raw_stack, *raw_moves = read_input.slice_before(/move/).to_a
 stacks = raw_stack.tap { _1.pop(2) }.map { _1.chars[(1.._1.length).step(4)] }.transpose.map { |s| s.reject { _1 == " " } }
 stacks2 = stacks.map(&:dup)
-moves = raw_moves.flatten.map { |m| m.match(/move (\d+) from (\d+) to (\d+)/) { { cnt: _1[1].to_i, from: _1[2].to_i - 1, to: _1[3].to_i - 1 } } }
+moves = raw_moves.flatten.map { |m| m.match(/move (\d+) from (\d+) to (\d+)/) { [_1[1].to_i, _1[2].to_i - 1, _1[3].to_i - 1] } }
 
 moves.each { move(stacks, _1, :stack) }
 
