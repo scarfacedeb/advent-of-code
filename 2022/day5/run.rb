785e93351683cf51e78953a9c925a8488b6765c9
Andrew Volozhanin <scarfacedeb@gmail.com>
2022-12-05 06:48:01 +0100

    2022 / Day 5

---
+++ 2022/day5/run.rb
@@ -0,0 +1,23 @@
+def read_input
+  file = $all ? "input.txt" : "example.txt"
+  File.read(ARGV.last || file).split("\n")
+end
+
+def move(stacks, m, order = nil)
+  stacks[m[:from]].shift(m[:cnt]).then { stacks[m[:to]].unshift(*(order == :stack ? _1.reverse : _1)) }
+end
+
+raw_stack, *raw_moves = read_input.slice_before(/move/).to_a
+stacks = raw_stack.tap { _1.pop(2) }.map { _1.chars[(1.._1.length).step(4)] }.transpose.map { |s| s.reject { _1 == " " } }
+stacks2 = stacks.map(&:dup)
+moves = raw_moves.flatten.map { |m| m.match(/move (\d+) from (\d+) to (\d+)/) { { cnt: _1[1].to_i, from: _1[2].to_i - 1, to: _1[3].to_i - 1 } } }
+
+moves.each { move(stacks, _1, :stack) }
+
+ans = stacks.map(&:first).join
+puts "Answer 5.1: #{ans}"
+
+moves.each { move(stacks2, _1) }
+ans2 = stacks2.map(&:first).join
+
+puts "Answer 5.2: #{ans2}"
