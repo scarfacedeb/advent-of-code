d8b7c145f7739601645785e253a3cf96f660e0a5
Andrew Volozhanin <scarfacedeb@gmail.com>
2022-12-09 10:25:59 +0100

    2022 / Day 9 / animation

--- 2022/day9/run.rb
+++ 2022/day9/run.rb
@@ -1,32 +1,41 @@
 require 'set'
 require 'debug'
+require 'tty-cursor'
 
 def read_input
   file = $all ? "input.txt" : "example.txt"
   File.read(ARGV.last || file).split("\n").map { |l| d, s = l.split(" "); [d.to_sym, s.to_i] }
 end
 
-def draw(rope)
-  max = rope.flatten.max
-  width = [max, 30].max
-  height = [max, 30].max
+$drawn_moves = []
+TICK = 0.005
+GRID_SIZE = 90
+
+def draw(rope, m)
+  $drawn_moves << m unless $drawn_moves.last == m
+
   grid = []
-
   marks = rope.map.with_index.each_with_object({}) { |(m, i), h| h[m] ||= i.zero? ? 'H' : i }
+  marks.merge! $drawn_visited.each_with_object({}) { |m, h| h[m] = '#' }
+  marks.transform_keys! { |m| [m[0] + 80, m[1] + 80] }
 
-  width.times do |y|
+  print $cursor.restore
+  print $cursor.save
+
+  GRID_SIZE.times do |y|
     row = []
-    height.times do |x|
+    GRID_SIZE.times do |x|
       point = marks[[x, y]]
+      point = "#" if $drawn_visited.include?([x, y])
       point ||= "s" if [x, y] == [0, 0]
       point ||= "."
       row << point
     end
     grid << row
   end
-  puts "\n"
-  puts grid.map { _1.join("") }.reverse.join("\n")
-  puts "\n"
+  print grid.map { _1.join("") }.reverse.join("\n")
+  # print "\n\n\nMOVE: #{$drawn_moves.map(&:join).join(' -> ')}"
+  sleep TICK
 end
 
 DIRS = {
@@ -34,13 +43,6 @@ DIRS = {
   L: [-1, 0],
   U: [0, 1],
   D: [0, -1]
-}
-
-DELTAS = {
-  R: 1,
-  L: -1,
-  U: 1,
-  D: -1
 }
 
 def decr_abs(int, sub = 1)
@@ -81,52 +83,48 @@ def move_tail(head, tail)
 end
 
 def move(rope, m, visited)
-  puts "MOVE: #{m.join}"
+  draw(rope, m)
+
   dir, steps = m
   delta = DIRS[dir]
+  last_drawn_rope = rope
 
   steps.times do
-    # binding.b
-
-    # head = rope.shift
     head = apply_delta(rope.shift, delta)
-    top_head = head
-    tail = nil
-    $new_rope = [head]
-    $rope = rope
+    rope = [head] + rope.map { |tail|
+      head = move_tail(head, tail)
+    }
+    visited << head
 
-    rope = rope.map.with_index(1) do |t, idx|
-      # puts "T#{idx}: #{t}"
-      tail = t
-      # head = apply_delta(head, delta)
-      tail = move_tail(head, tail)
-      # top_head = head if idx.zero?
-      # binding.b
-      $new_rope << tail
-      head = tail
-    end
-
-    rope.unshift(top_head)
-    # binding.b
-    visited << tail
+    # draw(rope, m) unless rope == last_drawn_rope
+    last_drawn_rope = rope
   end
-    draw(rope)
+
+  draw(rope, m)
+  sleep TICK
   rope
 end
 
+$cursor = TTY::Cursor
+print $cursor.clear_screen
+print $cursor.hide
+
 moves = read_input
+
+# visited = Set.new
+# rope = 2.times.map { [0, 0] }
+# moves.each { |m| rope = move(rope, m, visited) }
+# ans = visited.count
+
 visited = Set.new
-rope = 2.times.map { [0, 0] }
-
-moves.each { |m| rope = move(rope, m, visited) }
-
-ans = visited.count
-
-visited = Set.new
+$drawn_visited = visited
 rope = 10.times.map { [0, 0] }
 moves.each { |m| rope = move(rope, m, visited) }
 
 ans2 = visited.count
 
-puts "Answer 9.1: #{ans}"
+print "\n\n"
+# puts "Answer 9.1: #{ans}"
 puts "Answer 9.2: #{ans2}"
+
+print $cursor.show
