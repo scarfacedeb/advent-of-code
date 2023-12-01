9c5dabc9d59397079853ed7f5165ae0e27460582
Andrew Volozhanin <scarfacedeb@gmail.com>
2023-12-01 23:30:00 +0100

    2023 / Day 1

---
+++ 2023/day1/run.rb
@@ -0,0 +1,28 @@
+lines = File.readlines("example.txt")
+
+sum = lines.map { |line|
+  nums = line.scan(/\d/)
+  (nums[0] + nums[-1]).to_i
+}.sum
+
+puts "Day 1.1: #{sum}"
+
+NUMS = %w{one two three four five six seven eight nine}.map.with_index(1).to_h
+
+lines = File.readlines("data.txt")
+# lines = ["rzvlkjvone142oneightpv"]
+
+sum = lines.map { |line|
+  nums = {}
+
+  NUMS.each do |num, i|
+    line.gsub(num) { nums[Regexp.last_match.begin(0)] = i.to_s }
+  end
+
+  line.gsub(/\d/) { nums[Regexp.last_match.begin(0)] = _1.to_s }
+
+  nums = nums.sort_by(&:first).map(&:last)
+  (nums[0] + nums[-1]).to_i
+}.sum
+
+puts "Day 1.2: #{sum}"
